515ccba061221a347c1be9774af4b3277b35b9f4
Elliott Landsborough <elliott.landsborough@gmail.com>
2020-12-04 23:26:05 +0000

    Provision

---
+++ provision.sh
@@ -0,0 +1,20 @@
+# unfinished
+docker run --name auralist -t -d ubuntu:latest
+docker exec -ti auralist /bin/bash
+export DEBIAN_FRONTEND=noninteractive
+apt update
+apt upgrade -y
+apt install -y git wget make
+
+cd
+wget https://golang.org/dl/go1.15.6.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.15.6.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=~/go
+go version
+
+
+cd $GOPATH/src/github.com/ElliottLandsborough/auralist
+make deps
+make files
+nano config.yml
